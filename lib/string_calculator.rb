038d669312fe033f8728d7b093481279e6ed3166
Mayur Kambariya <mayurkambariya@gmail.com>
2024-05-20 11:37:38 +0530

    return 0 if string is empty

--- lib/string_calculator.rb
+++ lib/string_calculator.rb
@@ -1,2 +1,5 @@
 class StringCalculator
+  def add input_str
+    0 if input_str.empty?
+  end
 end
--- spec/string_calculator_spec.rb
+++ spec/string_calculator_spec.rb
@@ -1,6 +1,11 @@
 require_relative '../lib/string_calculator'
 
 RSpec.describe StringCalculator do
+  subject(:string_calculator) { described_class.new }
+
   describe '#add' do
+    it '0 for an empty string' do
+      expect(string_calculator.add('')).to eq 0
+    end
   end
 end
